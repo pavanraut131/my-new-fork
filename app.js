cb5fd517872aa37094dcdc8f37711a2df28b557b
bhanudevc <bhanudevchauhan@gmail.com>
2025-02-01 09:01:47 +0530

    after stashed

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 console.log("first commit ");
+let a = 30;
+let b = 20;
+
+function sum(a, b) {
+  return a + b;
+}
+
+console.log(sum(a, b));
+
+function greeting() {
+  console.log("hello");
+}
+greeting();
