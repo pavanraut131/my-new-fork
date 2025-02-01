fe05f8d52ebaa9f1030dc514dd527d3ae6369eaa
Vaghasiyakeyur <keyurpatel2522@gmail.com>
2025-02-01 10:48:16 +0530

    add sub function to app.js file

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ function sum(a, b) {
 
 console.log(sum(a, b));
 
+function sub(a, b) {
+  return a - b;
+}
+console.log(sub(15, 11));
+
 console.log("this is new greeting ");
 console.log("this is greeting 2");
-ghh;
-jjdbsgfgti git
